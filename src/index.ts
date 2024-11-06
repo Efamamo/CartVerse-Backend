6790f79720a5e76c75f29c6e73b656689f68b87e
ephrem <ephremmamo555@gmail.com>
2024-11-06 14:13:31 +0300

    add: integration with database

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import cors from 'cors';
+import { connectToDB } from './database/mongoose';
 
 const app = express();
 app.use(cors());
+connectToDB();
 
 app.get('/', (req, res) => {
   res.json({ message: 'Hello User' });
 });
 
-app.listen(3000);
+app.listen(3001);
